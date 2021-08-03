da762d4cb48ebef1fbf213e243c4f924d1338b05
edielam <edguard738@gmail.com>
2021-08-03 11:42:14 +0000

    redifined the output command

--- HackBioTask1.R
+++ HackBioTask1.R
@@ -1,7 +1,9 @@
+#!/bin/Rscript
+
 name<- 'Shruti_Poojary'
 email<- "Poojaryshruti27@gmail.com"
 username<- "@Shruti"
 biostack<- "Data Analysis"
 output<- c(name,email,username,biostack)
-print(output)
 
+cat(paste(output, collapse = ','))
