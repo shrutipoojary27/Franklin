89881dbdc2f5dd1a0e540f98f645f8642dc18c7d
Shruti <poojaryshruti27@gmail.com>
2021-08-05 20:51:15 +0530

    added hamming code

--- Frank_Shruti.R
+++ Frank_Shruti.R
@@ -4,6 +4,8 @@ name<- 'Shruti_Poojary'
 email<- "Poojaryshruti27@gmail.com"
 username<- "@Shruti"
 biostack<- "Data Analysis"
-output<- c(name,email,username,biostack)
+twitter<- "@Shruti"
+output<- c(name,email,username,biostack,twitter)
 
 cat(paste(output, collapse = ','))
+sum(username != twitter)
