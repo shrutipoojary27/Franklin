336a69e8a341838ee8730e69f2d57f45e627b17a
edielam <edguard738@gmail.com>
2021-08-06 17:39:15 +0000

    fixed hamming algo

--- C_file_Eddie.c
+++ C_file_Eddie.c
@@ -3,7 +3,7 @@
 int hammingDist(char *str1, char *str2)
 {
     int i = 0, count = 0;
-    while (str1[i] != '\0')
+    while (str1[i] != '\0' || str2[i] != '\0')
     {
         if (str1[i] != str2[i])
             count++;
