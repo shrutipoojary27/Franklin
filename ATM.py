7b838481e08a3e854b56bb8deb3bb19a5445cef3
ATM1244 <88340997+ATM1244@users.noreply.github.com>
2021-08-06 22:35:29 +0530

    it's done

--- ATM.py
+++ ATM.py
@@ -1,1 +1,18 @@
-print("Atreyi Mandal, mandalatreyi@gmail.com, @ATM,Biotechnolgy")
+def ham(slack_username,twitter_username ):
+    i=0
+    count=0
+    while(i<len(slack_username)):
+        if slack_username[i]!=twitter_username[i]:
+            count += 1
+        i += 1
+    return count
+
+name = "Atreyi MAndal"
+email = "mandalatreyi@gmail.com"
+biostack ="Biotechnology"
+slack_username = "@ATM"
+twitter_username = "@Atm"
+d =  ham(slack_username,twitter_username)
+
+print("{},{},{},{},{},{}".format(name, email, slack_username, biostack, twitter_username, d))
+
