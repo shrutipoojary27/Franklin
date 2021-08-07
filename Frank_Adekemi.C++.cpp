9c26d7717a85d9e573ea825dd201dd3631f025eb
Adekemi <88344075+Aadekemi@users.noreply.github.com>
2021-08-07 09:10:39 +0100

    Add files via upload

---
+++ Frank_Adekemi.C++.cpp
@@ -0,0 +1,35 @@
+This program is for printing the Name, EmailID, Langauge used, Biostack and Slack ID using C++ programming Language using BASH to CSV file
+
+#include <iostream>
+using namespace std;
+//
+int main()
+{
+    cout << "NAME:Adepeju Adekemi\nE-MAIL: aadepeju1206@gmail.com\nUSED LANGUAGE: C++\nBIOSTACK: Genomics\nTWITTERUSERNAME:@Adekems\nSLACKUSERNAME: @Adekemi\n";
+    return 0;
+ 
+    // C++ program to find hamming distance b/w
+// two string
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// function to calculate Hamming distance
+int hammingDist(char *str1, char *str2)
+{
+    int i = 0, count = 0;
+    while (str1[i] != '\0')
+    {
+        if (str1[i] != str2[i])
+            count++;
+        i++;
+    }
+    return count;
+}
+ 
+// driver code
+int main()
+{
+  // function call
+    cout << hammingDist (SLACKUSERNAME;@Adekemi, TWITTERUSERNAME:Adekems);
+ 
+    return 0;
